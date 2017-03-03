eabd2bbdcbdbc4c21aacfb60af5b1829337717b1
Andrew Ferrari <andrew.c.ferrari17@gmail.com>
2017-03-02 21:01:06 -0500

    menu cleanup and debug improvements
    the phonehomemenu cleaned up with new showMenu function. global
    phonebook.DEBUG implemented so that a global debug setting can be
    toggled via phonebook.changeDebugMode()

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # phonebook
 shit tier phonebook to relearn java
+testing with a menu, loops, lists, files, and other things
--- src/phonebook/phonebook.java
+++ src/phonebook/phonebook.java
@@ -1,34 +1,31 @@
 package phonebook;
 
 import java.util.Scanner;
+import java.util.List;
 
 public class phonebook {
-
+	public static boolean DEBUG = false;
 	public static void main(String[] args) {
-		boolean DEBUG = false;
-		phonehomemenu(DEBUG);
+		phonehomemenu();
 		System.out.println("quitting from main");
+		if (DEBUG == true){
+			System.out.println("exited with DEBUG = "+DEBUG);
+		}
 	}
 
-	public static void phonehomemenu(boolean debug) {
+	public static void phonehomemenu() {
 		Scanner menuScan = new Scanner(System.in);
 		int full = 1;
 		int homemenuChoice = -1;
 		while (full == 1) {
-			System.out.println("0. Change Debug Mode");
-			System.out.println("1. Add Entry");
-			System.out.println("2. Remove Entry");
-			System.out.println("3. Display Entry");
-			System.out.println("4. Display All Entries");
-			System.out.println("5. Quit");
-			System.out.print("Choice: ");
+			showMenu();
 			homemenuChoice = menuScan.nextInt();
 			System.out.println();
 
 			switch (homemenuChoice) {
 
 			case 0:
-				debug = changeDebugMode();
+				changeDebugMode();
 				break;
 			case 1:
 				addEntry();
@@ -37,12 +34,6 @@ public class phonebook {
 				removeEntry();
 				break;
 			case 3:
-				/*
-				 * do we search? request specific? if we search, do we have a
-				 * separate search case plus have a case for display an entry
-				 * you know the name of? what if I only know one attribute? ->
-				 * can still show the value
-				 */
 				displayEntry();
 				break;
 			case 4:
@@ -57,15 +48,31 @@ public class phonebook {
 		}
 		menuScan.close();
 	}
-	
-	public static void showMenu(){
-		
+
+	public static void showMenu() {
+		if (DEBUG == true){
+			System.out.println("entered showMenu");
+		}
+		System.out.println("0. Change Debug Mode");
+		System.out.println("1. Add Entry");
+		System.out.println("2. Remove Entry");
+		System.out.println("3. Display Entry");
+		System.out.println("4. Display All Entries");
+		System.out.println("5. Quit");
+		//System.out.print("Choice: ");
+		if (DEBUG == true){
+			System.out.println("exiting showMenu");
+			System.out.print("Choice: ");
+		}else System.out.print("Choice: ");
 	}
 
-	public static boolean changeDebugMode() {
+	public static void changeDebugMode() {
+		if (DEBUG == true){
+			System.out.println("entered changeDebugMode");
+		}
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
-		boolean debug = false;
+		//boolean debug = false;
 		char runDch;
 		String runAsk = "r";
 		System.out.print("Enter runmode [r],d: ");
@@ -76,31 +83,40 @@ public class phonebook {
 
 		if (runDch == 'd') {
 			System.out.println("DEBUG mode ENABLED\n");
-			debug = true;
+			DEBUG = true;
 		} else if (runDch == 'r') {
 			System.out.println("DEBUG mode DISABLED\n");
-			debug = false;
+			DEBUG = false;
 		} else {
-			debug = false;
+			DEBUG = false;
 			System.out.println("incorrect value input. use r or d. defaulting to r");
+			System.out.println("DEBUG mode DISABLED\n");
 		}
 		// sc.close();
-		return debug;
+		if (DEBUG == true){
+			System.out.println("exiting showMenu");
+		}
+		
 	}
 
 	public static void addEntry() {
-
+		System.out.println("no code");
 	}
 
 	public static void removeEntry() {
-
+		System.out.println("no code");
 	}
 
 	public static void displayEntry() {
-
+		/*
+		 * do we search? request specific? if we search, do we have a separate
+		 * search case plus have a case for display an entry you know the name
+		 * of? what if I only know one attribute? -> can still show the value
+		 */
+		System.out.println("no code");
 	}
 
 	public static void displayAll() {
-
+		System.out.println("no code");
 	}
 }
